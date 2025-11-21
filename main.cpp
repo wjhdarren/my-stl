9ace9e5abcbb09c100ca5aae40b71de91b36ab4f
Darren Weng <wjhdarren@gmail.com>
2025-11-21 22:07:26 +0800

    refactor main.cpp to manually test things meanwhile keep it simple
    * main.cpp:

--- main.cpp
+++ main.cpp
@@ -11,20 +11,35 @@ namespace stdv = std::ranges::views;
 namespace stdr = std::ranges;
 using Map = my::unordered_map<std::string, std::string>;
 
-int main() {
+void test_vector() {
+  // create from view
   auto vec = my::vector<int>(std::from_range, stdv::iota(0, 10));
   vec.show();
 
-  auto str_v = std::views::transform(stdv::iota(0, 10),
+  // create from view of string
+  auto str_v = stdv::transform(stdv::iota(0, 10),
                                      [](int i) { return std::to_string(i); });
   auto vec2 = my::vector<std::string>(std::from_range, str_v);
   vec2.show();
 
-  auto unique_ptr_v = std::views::transform(
+  // create from view of unique_ptr (don't have copy ctor)
+  auto unique_ptr_v = stdv::transform(
       stdv::iota(0, 10), [](int i) { return std::make_unique<int>(i); });
+
   auto vec3 = my::vector<std::unique_ptr<int>>(std::from_range, unique_ptr_v);
   vec3.show();
 
+  // use with pipe operator & template deduction
   auto vec4 = vec | stdv::filter([](int i) { return i <= 5;}) | stdr::to<my::vector>();
   vec4.show();
+
+}
+
+void test_unique_ptr() {
+
+}
+
+int main() {
+    test_vector();
+    test_unqiue_ptr();
 }
